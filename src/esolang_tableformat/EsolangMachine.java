d25d1eb4904688069ba7f82b7d238853bf78dcd0
MrRedstoner <pato.grman@gmail.com>
2018-07-13 10:42:58 +0200

    Machine work
    The machine should be done for the time being, will be tested once support structure is available

--- src/esolang_tableformat/EsolangMachine.java
+++ src/esolang_tableformat/EsolangMachine.java
@@ -8,7 +8,27 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 public class EsolangMachine {
 	private enum Direction{
-		UP,DOWN,LEFT,RIGHT
+		UP,DOWN,LEFT,RIGHT;
+		
+		static Direction clockwise(Direction d){
+			switch(d){
+			case DOWN:return LEFT;
+			case LEFT:return UP;
+			case RIGHT:return DOWN;
+			case UP:return RIGHT;
+			}
+			return d;
+		}
+		
+		static Direction counterClockwise(Direction d){
+			switch(d){
+			case DOWN:return RIGHT;
+			case LEFT:return DOWN;
+			case RIGHT:return UP;
+			case UP:return LEFT;
+			}
+			return d;
+		}
 	}
 	
 	private class Memory{
@@ -53,12 +73,12 @@ public class EsolangMachine {
 		special=Special.NONE;
 	}
 	
-	void step(){
+	boolean step(){//returns true for halt
 		if(special==Special.PAUSE){
 			if(System.currentTimeMillis()>=pauseUntil){
 				special=Special.NONE;
 			}
-			return;
+			return false;
 		}
 		
 		XSSFCell cell=sheet.getRow(position[0]).getCell(position[1]);
@@ -66,6 +86,7 @@ public class EsolangMachine {
 		
 		if(special==Special.NEXT_AS_NUMBER){
 			memory.a=value;
+			special=Special.NONE;
 		}else{
 			switch(value){
 				case 0:{//do nothing
@@ -73,61 +94,130 @@ public class EsolangMachine {
 				case 1:{//go up
 					direction=Direction.UP;
 				}break;
-				case 2:{//
-					
+				case 2:{//go down
+					direction=Direction.DOWN;
 				}break;
-				case 3:{//
-					
+				case 3:{//go left
+					direction=Direction.LEFT;
 				}break;
-				case 4:{//
-					
+				case 4:{//go right
+					direction=Direction.RIGHT;
 				}break;
-				case 5:{//
-					
+				case 5:{//next as number
+					special=Special.NEXT_AS_NUMBER;
 				}break;
-				case 6:{//
-					
+				case 6:{//input number		
+					memory.a=in.readNumber();
 				}break;
-				/*
-go down
-go left
-go right
-next as number
-input number
-input character
-output number
-output character
-move to B
-move from B
-move to C
-move from C
-move to D
-move from D
-move from E
-move to F
-move from F
-move to G
-move from G
-move to H
-move from H
-move to I
-move from I
-move to J
-move from J
-negate
-and
-or
-xor
-add
-substract
-multiply
-divide
-modulo
-switch
-pause
-end
-
-				 */
+				case 7:{//input character
+					memory.a=in.readCharacter();
+				}break;
+				case 8:{//output number
+					out.writeNumber(memory.a);
+				}break;
+				case 9:{//output character
+					out.writeCharacter(memory.a);
+				}break;
+				case 10:{//move to B
+					memory.b=memory.a;
+				}break;
+				case 11:{//move from B
+					memory.a=memory.b;
+				}break;
+				case 12:{//move to C
+					memory.c=memory.a;
+				}break;
+				case 13:{//move from C
+					memory.a=memory.c;
+				}break;
+				case 14:{//move to D
+					memory.d=memory.a;
+				}break;
+				case 15:{//move from D
+					memory.a=memory.d;
+				}break;
+				case 16:{//move from E
+					memory.a=memory.e;
+				}break;
+				case 17:{//move to F
+					memory.f[memory.b]=memory.a;
+				}break;
+				case 18:{//move from F
+					memory.a=memory.f[memory.b];
+				}break;
+				case 19:{//move to G
+					memory.g.add(memory.a);
+				}break;
+				case 20:{//move from G
+					if(!memory.g.isEmpty()){
+						memory.a=memory.g.pop();
+					}
+				}break;
+				case 21:{//move to H
+					memory.h.add(memory.a);
+				}break;
+				case 22:{//move from H
+					if(!memory.h.isEmpty()){
+						memory.a=memory.h.pop();
+					}
+				}break;
+				case 23:{//move to I
+					memory.i.add(memory.a);
+				}break;
+				case 24:{//move from I
+					if(!memory.i.isEmpty()){
+						memory.a=memory.i.pop();
+					}
+				}break;
+				case 25:{//move to J
+					memory.j.add(memory.a);
+				}break;
+				case 26:{//move from J
+					if(!memory.j.isEmpty()){
+						memory.a=memory.j.pop();
+					}
+				}break;
+				case 27:{//negate
+					memory.e=memory.c^Integer.MAX_VALUE;
+				}break;
+				case 28:{//and
+					memory.e=memory.c&memory.d;
+				}break;
+				case 29:{//or
+					memory.e=memory.c|memory.d;
+				}break;
+				case 30:{//xor
+					memory.e=memory.c^memory.d;
+				}break;
+				case 31:{//add
+					memory.e=memory.c+memory.d;
+				}break;
+				case 32:{//subtract
+					memory.e=memory.c-memory.d;
+				}break;
+				case 33:{//multiply
+					memory.e=memory.c*memory.d;
+				}break;
+				case 34:{//divide
+					memory.e=memory.c/memory.d;
+				}break;
+				case 35:{//modulo
+					memory.e=memory.c%memory.d;
+				}break;
+				case 36:{//switch
+					if(memory.a>0){
+						direction=Direction.clockwise(direction);
+					}else if(memory.a<0){
+						direction=Direction.counterClockwise(direction);
+					}
+				}break;
+				case 37:{//pause
+					special=Special.PAUSE;
+					pauseUntil=System.currentTimeMillis()+memory.a;
+				}break;
+				case 38:{//end
+					return true;
+				}
 			}
 		}
 		
@@ -145,5 +235,7 @@ end
 				position[1]--;
 			}break;
 		}
+		
+		return false;
 	}
 }
